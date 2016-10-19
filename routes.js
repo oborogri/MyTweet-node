8db15f467c0597ee72054b04d15df6a5f80d9836
oborogri <grigore.oboroceanu@gmail.com>
2016-10-19 18:50:00 +0100

    MyTweet_v_01(User model and persistence implemented)

--- routes.js
+++ routes.js
@@ -10,10 +10,12 @@ module.exports = [
   { method: 'GET', path: '/', config: Accounts.main },
   { method: 'GET', path: '/signup', config: Accounts.signup },
   { method: 'GET', path: '/login', config: Accounts.login },
-  { method: 'POST', path: '/login', config: Accounts.authenticate },
-  { method: 'POST', path: '/register', config: Accounts.register },
+  { method: 'GET', path: '/logout', config: Accounts.logout },
 
   { method: 'GET', path: '/home', config: Tweets.home },
+
+  { method: 'POST', path: '/register', config: Accounts.register },
+  { method: 'POST', path: '/login', config: Accounts.authenticate },
 
   {
     method: 'GET',
