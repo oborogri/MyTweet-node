b40e405021255b754d5944bdc1dd3c68bb3a937a
oborogri <grigore.oboroceanu@gmail.com>
2016-12-20 12:55:05 +0000

    Users api tests implemented

--- test/tweetsapitest.js
+++ test/tweetsapitest.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const assert = require('chai').assert;
+var request = require('sync-request');
 
 suite('Tweet API tests', function () {
 
   test('get tweets', function () {
 
-    assert.equal(1, 1);
-
+    const url = 'http://localhost:4000/api/tweets';
+    var res = request('GET', url);
+    const tweets = JSON.parse(res.getBody('utf8'));
+    assert.equal(4, tweets.length);
   });
 });
