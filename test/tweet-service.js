f6e816e9382074c1b7e0fed0eba6d0ce4a85d146
oborogri <grigore.oboroceanu@gmail.com>
2016-12-20 18:40:06 +0000

    Tweet-service.js and sync-http-service.js api test classes implemented

---
+++ test/tweet-service.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const SyncHttpService = require('./sync-http-service');
+const baseUrl = 'http://localhost:4000';
+
+class TweetService {
+
+  constructor(baseUrl) {
+    this.httpService = new SyncHttpService(baseUrl);
+  }
+
+  getTweets() {
+    return this.httpService.get('/api/tweets');
+  }
+
+  getTweet(id) {
+    return this.httpService.get('/api/tweets/' + id);
+  }
+
+  createTweet(newTweet) {
+    return this.httpService.post('/api/tweets', newTweet);
+  }
+
+  getUsers() {
+    return this.httpService.get('/api/users');
+  }
+
+  getUser(id) {
+    return this.httpService.get('/api/users/' + id);
+  }
+
+  createUser(newUser) {
+    return this.httpService.post('/api/users', newUser);
+  }
+}
+
+module.exports = TweetService;
--- test/usersapitest.js
+++ test/usersapitest.js
@@ -1,101 +1,24 @@
 'use strict';
 
 const assert = require('chai').assert;
-var request = require('sync-request');
+const TweetService = require('./tweet-service');
+const fixtures = require('./fixtures.json');
 
 suite('User API tests', function () {
 
-  test('get users', function () {
+  let users = fixtures.users;
+  let newUser = fixtures.newUser;
 
-    const url = 'http://localhost:4000/api/users';
-    var res = request('GET', url);
-    const users = JSON.parse(res.getBody('utf8'));
-    assert.equal(4, users.length);
+  const tweetService = new TweetService('http://localhost:4000');
 
-    assert.equal(users[0].firstName, 'Homer');
-    assert.equal(users[0].lastName, 'Simpson');
-    assert.equal(users[0].posts, 4);
-
-    assert.equal(users[1].firstName, 'Marge');
-    assert.equal(users[1].lastName, 'Simpson');
-    assert.equal(users[1].posts, 2);
-
-    assert.equal(users[2].firstName, 'Bart');
-    assert.equal(users[2].lastName, 'Simpson');
-    assert.equal(users[2].posts, 1);
-
-    assert.equal(users[3].firstName, 'Lisa');
-    assert.equal(users[3].lastName, 'Simpson');
-    assert.equal(users[3].posts, 1);
-
-  });
-
-  test('get one user', function () {
-
-    const allUsersUrl = 'http://localhost:4000/api/users';
-    var res = request('GET', allUsersUrl);
-    const users = JSON.parse(res.getBody('utf8'));
-
-    const oneUserUrl = allUsersUrl + '/' + users[0]._id;
-    res = request('GET', oneUserUrl);
-    const oneUser = JSON.parse(res.getBody('utf8'));
-
-    assert.equal(oneUser.firstName, 'Homer');
-    assert.equal(oneUser.lastName, 'Simpson');
-    assert.equal(oneUser.posts, 4);
-
-  });
-
-  test('create a user', function () {
-
-    const usersUrl = 'http://localhost:4000/api/users';
-    const newUser = {
-      firstName: 'Barnie',
-      lastName: 'Grumble',
-      email: 'barnie@simpson.com',
-      password: 'secret',
-      joined: 'Tue, Dec 20th, 2016',
-      posts: 3,
-    };
-
-    const res = request('POST', usersUrl, { json: newUser });
-    const returnedUser = JSON.parse(res.getBody('utf8'));
-    const users = JSON.parse(res.getBody('utf8'));
-
-    assert.equal(returnedUser.firstName, 'Barnie');
-    assert.equal(returnedUser.lastName, 'Grumble');
-    assert.equal(returnedUser.email, 'barnie@simpson.com');
-    assert.equal(returnedUser.password, 'secret');
-    assert.equal(returnedUser.joined, 'Tue, Dec 20th, 2016');
-    assert.equal(returnedUser.posts, 3);
-
-    const url = 'http://localhost:4000/api/users';
-    var resUsers = request('GET', url);
-    const usersAll = JSON.parse(resUsers.getBody('utf8'));
-    assert.equal(5, usersAll.length);
-  });
-
-  test('delete one user', function () {
-
-    const allUsersUrl = 'http://localhost:4000/api/users';
-    var res = request('GET', allUsersUrl);
-    const users = JSON.parse(res.getBody('utf8'));
-    const oneUserUrl = allUsersUrl + '/' + users[0]._id;
-    var res = request('DELETE', oneUserUrl);
-
-    var resAll = request('GET', allUsersUrl);
-    const usersAll = JSON.parse(resAll.getBody('utf8'));
-    assert.equal(usersAll.length, 4);
-  });
-
-  test('delete all users', function () {
-
-    const allUsersUrl = 'http://localhost:4000/api/users';
-    var resDelete = request('DELETE', allUsersUrl);
-
-    var resAll = request('GET', allUsersUrl);
-    const usersAll = JSON.parse(resAll.getBody('utf8'));
-    assert.equal(usersAll.length, 0);
+  test('create an user', function () {
+    const returnedUser = tweetService.createUser(newUser);
+    assert.equal(returnedUser.firstName, newUser.firstName);
+    assert.equal(returnedUser.lastName, newUser.lastName);
+    assert.equal(returnedUser.email, newUser.email);
+    assert.equal(returnedUser.password, newUser.password);
+    assert.equal(returnedUser.joined, newUser.joined);
+    assert.equal(returnedUser.posts, newUser.posts);
+    assert.isDefined(returnedUser._id);
   });
 });
-
