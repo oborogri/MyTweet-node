4ee997f10353217151837cff11428899c8684ab1
oborogri <grigore.oboroceanu@gmail.com>
2016-12-26 12:29:02 +0000

    User home timeline modified to include followed users tweets

--- app/controllers/friendships.js
+++ app/controllers/friendships.js
@@ -12,19 +12,23 @@ exports.follow = {
         sourceUserFollowing = sourceUser.following;
         const sourceUserId = sourceUser.id;
         const targetUserId = request.payload.targetUser;
-        if ((sourceUserId != targetUserId) && (sourceUserFollowing.indexOf(targetUserId) != -1)) {
-          sourceUserFollowing.push(targetUserId);//add target user to following array
+        if ((sourceUserId !== targetUserId) && (sourceUserFollowing.indexOf(targetUserId) === -1)) {
+          sourceUserFollowing.push(targetUserId);//add target user to source user following array
 
-          User.findOne({ _id: { $in: targetUser } }).then(targetUser => {
+          User.findOne({ _id: targetUserId }).then(targetUser => {
             targetUserFollowedBy = targetUser.followedBy;
-            targetUserFollowedBy.push(sourceUser.id);//add source user to followedBy array
+            if ((sourceUserId !== targetUserId) && (targetUserFollowedBy.indexOf(sourceUserId) === -1)) {
+              targetUserFollowedBy.push(sourceUserId);//add source user to target user followed by array
+            } else {
+              console.log('Already followed by this user');
+            }
+
+            sourceUser.save();
             return targetUser.save();
           });
         } else {
           console.log('Already following this user');
-        }
-
-        return sourceUser.save();
+        };
       }).then(response => {
         reply.redirect('/home');
       }).catch(err => {
